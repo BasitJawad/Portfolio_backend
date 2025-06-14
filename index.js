30bd71d80eaafd6397cb3c700bdfdaa132bf28ff
Basit <muhbasit235@gmail.com>
2025-06-14 14:52:12 +0300

    Updated the security

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ dotenv.config();
 require('./src/Database/connection');
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.set('trust proxy', 1); // Required for secure cookies on Railway
 
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://basitjawaddev.netlify.app"
   ],
-  credentials: true // if you're using cookies or authorization headers
+  credentials: true
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
 
 const userRoute = require('./src/Routes/userRoute');
