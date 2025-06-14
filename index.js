0bdf4d921c4f740a2eb74c11e3b6055e8e6a3339
Basit <muhbasit235@gmail.com>
2025-06-14 16:38:09 +0300

    added some live links

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ const dotenv = require('dotenv');
 const path = require('path');
 const cors = require("cors");
 dotenv.config();
-
 require('./src/Database/connection');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://basitjawaddev.netlify.app",
+  "https://portfolio-frontend-6f1b-eight.vercel.app",
+  "https://portfolio-frontend-6f1b-git-main-basitjawads-projects.vercel.app",
+  "https://portfolio-frontend-6f1b-hj2is1qv0-basitjawads-projects.vercel.app"
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://basitjawaddev.netlify.app"
-  ],
-  credentials: true // if you're using cookies or authorization headers
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
